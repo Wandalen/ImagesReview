f1e3b0f7f0706d3cd9c737ceb9a68f623aeb556f
Jackie <jackshel45@gmail.com>
2020-05-21 00:37:43 +0300

    testing indentation

--- TEST.md
+++
@@ -1,2 +0,0 @@
-HELLOO
-TEST line
--- proto/Index.js
+++ proto/Index.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+// testing
+const fs = require("fs");
 const wTools = require("wTools");
 require("wFiles");
 
@@ -13,15 +14,23 @@ function readYML(yml) {
 
 function writeMd(md, data) {
   wTools.fileProvider.fileWrite({
-      filePath: md,
-      data: JSON.stringify(data),
-    });
+    filePath: md,
+    data: JSON.stringify(data),
+  });
 }
 
-const {'Modules to read/write/convert/compress images': rwTableObj} = readYML(`${__dirname}/../data/readWriteConvertCompressImg.yml`);
-const {'Modules to process images' : processTableObj} = readYML(`${__dirname}/../data/processImg.yml`);
-const {'Awesome image!': header, "Columns' definitions": columnsDef, 'Sorting Algorithm (descending order significance)': algo} = readYML(`${__dirname}/../data/mainInfo.yml`);
-const {Resources: resources} = readYML(`${__dirname}/../data/resources.yml`);
+const { "Modules to read/write/convert/compress images": rwTableObj } = readYML(
+  `${__dirname}/../data/readWriteConvertCompressImg.yml`
+);
+const { "Modules to process images": processTableObj } = readYML(
+  `${__dirname}/../data/processImg.yml`
+);
+const {
+  "Awesome image!": header,
+  "Columns' definitions": columnsDef,
+  "Sorting Algorithm (descending order significance)": algo,
+} = readYML(`${__dirname}/../data/mainInfo.yml`);
+const { Resources: resources } = readYML(`${__dirname}/../data/resources.yml`);
 
 // console.log(rwTable['Modules to read/write/convert/compress images']);
 // console.log(processTableObj);
@@ -33,17 +42,23 @@ console.log(resources);
 // console.log('---');
 
 function tableObjToMd(table, obj) {
-  let temp = '';
-  
+  let temp = "";
+
   if (table === 1) {
-    temp = "### Modules to read/write/convert/compress images\n| **N** | **R** | **W** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps** |\n| --- | --- | --- | --- | --- | --- | -- | --- | --- | --- |\n";
-    obj.forEach(el => {
-      temp += `| [**${el.N.name}**](${el.N.link}) | ${el.R.toString()} | ${el.R.toString()} | ${el.Code} | ${el.Modular} | ${el.I} | ${el.PL} | ${el['B.s']} | ${el['N.s']} | ${el.Deps} |\n`
+    temp =
+      "### Modules to read/write/convert/compress images\n| **N** | **R** | **W** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps** |\n| --- | --- | --- | --- | --- | --- | -- | --- | --- | --- |\n";
+    obj.forEach((el) => {
+      temp += `| [**${el.N.name}**](${
+        el.N.link
+      }) | ${el.R.toString()} | ${el.R.toString()} | ${el.Code} | ${
+        el.Modular
+      } | ${el.I} | ${el.PL} | ${el["B.s"]} | ${el["N.s"]} | ${el.Deps} |\n`;
     });
   } else if (table === 2) {
-    temp = "### Modules to process images\n| **N** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps**|\n| --- | --- | --- | --- | --- | --- | --- | --- |\n"
-    obj.forEach(el=> {
-      temp += `| [**${el.N.name}**](${el.N.link}) | ${el.Code} | ${el.Modular} | ${el.I} | ${el.PL} | ${el['B.s']} | ${el['N.s']} | ${el.Deps} |\n`
+    temp =
+      "### Modules to process images\n| **N** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps**|\n| --- | --- | --- | --- | --- | --- | --- | --- |\n";
+    obj.forEach((el) => {
+      temp += `| [**${el.N.name}**](${el.N.link}) | ${el.Code} | ${el.Modular} | ${el.I} | ${el.PL} | ${el["B.s"]} | ${el["N.s"]} | ${el.Deps} |\n`;
     });
   }
 
@@ -52,9 +67,14 @@ function tableObjToMd(table, obj) {
 
 writeMd(`${__dirname}/../output/README.md`, tableObjToMd(1, rwTableObj));
 
-fs.writeFileSync(`${__dirname}/../output/READMEfs1.md`, tableObjToMd(1, rwTableObj));
-fs.writeFileSync(`${__dirname}/../output/READMEfs2.md`, tableObjToMd(2, processTableObj));
-
+fs.writeFileSync(
+  `${__dirname}/../output/READMEfs1.md`,
+  tableObjToMd(1, rwTableObj)
+);
+fs.writeFileSync(
+  `${__dirname}/../output/READMEfs2.md`,
+  tableObjToMd(2, processTableObj)
+);
 
 // function jsToMd(data) {
 //   // let table1md = '';
@@ -74,7 +94,6 @@ fs.writeFileSync(`${__dirname}/../output/READMEfs2.md`, tableObjToMd(2, processT
 //     "Resources": resources,
 //   } = data;
 
-
 //   output += tableObjToMd(rwcc);
 
 // }
@@ -87,6 +106,4 @@ fs.writeFileSync(`${__dirname}/../output/READMEfs2.md`, tableObjToMd(2, processT
 //   data: JSON.stringify(data),
 // });
 
-
-
-fs.writeFileSync('TEST.md', `HELLOO\nTEST line`);
+fs.writeFileSync("TEST.md", `HELLOO\nTEST line`);
