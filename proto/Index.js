f7225899517e3a8f934169ffdbedbd8026053eaa
Jackie <jackshel45@gmail.com>
2020-05-21 12:23:25 +0300

    Indendation

--- proto/Index.js
+++ proto/Index.js
@@ -1,22 +1,27 @@
 const _ = require("wTools");
 require("wFiles");
 
-const {
-  "Modules to read/write/convert/compress images": rwTableObj
-} = readYML("../data/readWriteConvertCompressImg.yml");
-const {
-  "Modules to process images": processTableObj
-} = readYML(
-  "../data/processImg.yml"
-);
-const {
-  "Awesome image!": header,
-  "Columns' definitions": columnsDef,
-  "Sorting Algorithm (descending order significance)": algo,
-} = readYML("../data/mainInfo.yml");
-const {
-  Resources: resources
-} = readYML("../data/resources.yml");
+const 
+  {
+    "Modules to read/write/convert/compress images": rwTableObj
+  } = readYML("../data/readWriteConvertCompressImg.yml");
+
+const 
+  {
+    "Modules to process images": processTableObj
+  } = readYML("../data/processImg.yml");
+
+const 
+  {
+    "Awesome image!": header,
+    "Columns' definitions": columnsDef,
+    "Sorting Algorithm (descending order significance)": algo,
+  } = readYML("../data/mainInfo.yml");
+
+  const 
+  {
+    Resources: resources
+  } = readYML("../data/resources.yml");
 
 // console.log(rwTable['Modules to read/write/convert/compress images']);
 // console.log(processTableObj);
@@ -25,54 +30,50 @@ console.log(columnsDef);
 // console.log('---')
 // console.log(algo);
 
-let final = `# Awesome image!\nCurated overview of awesome Javascript projects to read / write / convert / compress / process images of different formats.\n${tableObjToMd(
-  1,
-  rwTableObj
-)}\n${tableObjToMd(2, processTableObj)}\n${colsDefsToMd(columnsDef)}`;
+let final = `# Awesome image!\nCurated overview of awesome Javascript projects to read / write / convert / compress / process images of different formats.\n${tableObjToMd(1,rwTableObj)}\n${tableObjToMd(2, processTableObj)}\n${colsDefsToMd(columnsDef)}`;
 
 // Writing to files
 writeMd("../output/README.md", final);
 
-function abs() {
-  return _.path.s.join(__dirname, ...arguments);
-}
-
-function readYML(yml) {
-  const data = _.fileProvider.fileRead({
-    filePath: abs(yml),
-    encoding: "yaml",
-  });
-
-  return data;
-}
-
-function writeMd(md, data) {
-  _.fileProvider.fileWrite(abs(md), data);
-}
-
-function tableObjToMd(table, obj) {
-  let temp = "";
-
-  if (table === 1) {
-    temp =
-      "### Modules to read/write/convert/compress images\n| **N** | **R** | **W** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps** |\n| --- | --- | --- | --- | --- | --- | -- | --- | --- | --- |\n";
-    obj.forEach((el) => {
-      temp += `| [**${el.N.name}**](${el.N.link}) | ${el.R.join(
-        ", "
-      )} | ${el.R.join(", ")} | ${el.Code} | ${el.Modular} | ${el.I} | ${
-        el.PL
-      } | ${el["B.s"]} | ${el["N.s"]} | ${el.Deps} |\n`;
-    });
-  } else if (table === 2) {
-    temp =
-      "### Modules to process images\n| **N** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps**|\n| --- | --- | --- | --- | --- | --- | --- | --- |\n";
-    obj.forEach((el) => {
-      temp += `| [**${el.N.name}**](${el.N.link}) | ${el.Code} | ${el.Modular} | ${el.I} | ${el.PL} | ${el["B.s"]} | ${el["N.s"]} | ${el.Deps} |\n`;
-    });
+function abs() 
+  {
+    return _.path.s.join(__dirname, ...arguments);
   }
 
-  return temp;
-}
+function readYML(yml) 
+  {
+    const data = _.fileProvider.fileRead({
+      filePath: abs(yml),
+      encoding: "yaml",
+    });
+
+    return data;
+  }
+
+function writeMd(md, data) 
+  {
+    _.fileProvider.fileWrite(abs(md), data);
+  }
+
+function tableObjToMd(table, obj) 
+  {
+    let temp = "";
+
+    if (table === 1) {
+      temp = "### Modules to read/write/convert/compress images\n| **N** | **R** | **W** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps** |\n| --- | --- | --- | --- | --- | --- | -- | --- | --- | --- |\n";
+      
+      obj.forEach((el) => {
+        temp += `| [**${el.N.name}**](${el.N.link}) | ${el.R.join(", ")} | ${el.R.join(", ")} | ${el.Code} | ${el.Modular} | ${el.I} | ${el.PL} | ${el["B.s"]} | ${el["N.s"]} | ${el.Deps} |\n`;});
+    } else if (table === 2) {
+      
+      temp ="### Modules to process images\n| **N** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps**|\n| --- | --- | --- | --- | --- | --- | --- | --- |\n";
+      obj.forEach((el) => {
+        temp += `| [**${el.N.name}**](${el.N.link}) | ${el.Code} | ${el.Modular} | ${el.I} | ${el.PL} | ${el["B.s"]} | ${el["N.s"]} | ${el.Deps} |\n`;
+      });
+    }
+
+    return temp;
+  }
 
 function colsDefsToMd(cols)
   {
@@ -95,11 +96,12 @@ function colsDefsToMd(cols)
     return temp;
   }
 
-function resourcesToMd(res) {
-  let temp = "### Resources:\n";
-  res.forEach((el, i) => {
-    temp += `${i + 1}. [${el.Name}](${el.Link})\n`;
-  });
+function resourcesToMd(res) 
+  {
+    let temp = "### Resources:\n";
+    res.forEach((el, i) => {
+      temp += `${i + 1}. [${el.Name}](${el.Link})\n`;
+    });
 
-  return temp;
-}
+    return temp;
+  }
