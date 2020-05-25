891dc850e8bd380bd37ff119b545b4470bfbcd8b
Jackie <jackshel45@gmail.com>
2020-05-25 16:19:41 +0300

    Changed image

--- sample/sharp/ReadPixel.js
+++ sample/sharp/ReadPixel.js
@@ -8,4 +8,4 @@ async function extractPix( path )
   console.log( 'rgb(' + data.toJSON().data.slice( 0, 3 ) + ')' );
 }
 // prints rgb(255,0,0)
-extractPix( `${__dirname}/../../data/Input.png` );
+extractPix( `${__dirname}/../../data/Image.png` );
