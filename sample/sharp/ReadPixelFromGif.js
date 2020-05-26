9707c39cce6484e6fa3dfb19b4c894c454fa3468
Jackie <jackshel45@gmail.com>
2020-05-26 12:57:44 +0300

    Minor

--- sample/sharp/ReadPixelFromGif.js
+++ sample/sharp/ReadPixelFromGif.js
@@ -3,4 +3,4 @@ const sharp = require( 'sharp' );
 const { extractPix } = require( './ReadPixel' );
 
 extractPix( `${__dirname}/../../data/images/Image.gif` );
-// OUTPUT: rgb(51,85,51)
+// OUTPUT: rgb(255,0,0)
