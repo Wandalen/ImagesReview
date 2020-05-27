1ed004ddeb0e5c8ad548564c0eb3467a4ebc188f
Jackie <jackshel45@gmail.com>
2020-05-27 14:37:36 +0300

    Updating dependents

--- proto/updateDeps.js
+++ proto/updateDeps.js
@@ -4,59 +4,43 @@ const wNpm = require( 'wnpmtools' );
 
 const { readYML, abs } = require( './Index' );
 
-const { 'Modules to read/write/convert/compress images' : table } = readYML( '../data/readWriteConvertCompressImg.yml' );
+let { 'Modules to read/write/convert/compress images' : tableYML } = readYML( '../data/readWriteConvertCompressImg.yml' );
+let moduleNames = tableYML.filter( ( el ) => el.Deps !== '-' ).map( ( el ) => el.N.name );
 
-let moduleDeps = {}
-let moduleNames = [ 'sharp', 'jimp', 'lwip', 'image-js', 'imagetracerjs', 'image-conversion', 'compressor', 'caman', 'capa_image_lib', 'gm' ]
+udpateDeps( moduleNames );
 
-// console.log( table );
-// getDeps( 'sharp' ).then( ( data ) => moduleDeps.sharp = data );
-// getDeps( 'jimp' ).then( ( data ) => moduleDeps.jimp = data );
-// getDeps( 'lwip' ).then( ( data ) => moduleDeps.lwip = data );
-// getDeps( 'image-js' ).then( ( data ) => moduleDeps[ 'image-js' ] = data );
-// getDeps( 'imagetracerjs' ).then( ( data ) => moduleDeps.imagetracerjs = data );
-// getDeps( 'image-conversion' ).then( ( data ) => moduleDeps[ 'image-conversion' ] = data );
-// getDeps( 'compressor' ).then( ( data ) => moduleDeps.compressor = data );
-// getDeps( 'caman' ).then( ( data ) => moduleDeps.caman = data );
-// getDeps( 'capa_image_lib' ).then( ( data ) => moduleDeps.capa_image_lib = data );
-// // getDeps( 'sharp' ).then( ( data ) => moduleDeps.sharp = data );
-// getDeps( 'gm' ).then( ( data ) => moduleDeps.gm = data )
-// .then( ( a ) => console.log( moduleDeps ) );
-
-async function getDeps( name )
+function getDeps( name )
 {
-  let res = await wNpm.npm.dependantsRetrieve( name );
-  // let data = await res;
-  // console.log( res );
-  return res;
+  return wNpm.npm.dependantsRetrieve( name );
 }
 
-function fillTable( arr )
+function createNameDepsObj( arr )
 {
-
-  let result;
-  let moduleDeps = {};
-  arr.forEach( async ( el ) =>
+  let results = arr.map( async ( el ) =>
   {
-    result = await getDeps( el )
-    moduleDeps = { ... moduleDeps, [ el ] : result }
-  } );
+    return { name : el, deps : await getDeps( el ) }
+  } )
 
-  return moduleDeps;
+  return results;
 }
 
-// const res = fillTable( moduleNames );
-// console.log( res )
-// console.log( moduleDeps )
-
-// setTimeout( () => console.log( res ), 1000 )
-// setTimeout( () => console.log( moduleDeps ), 1000 )
-
-async function udpateDeps( arr )
+function udpateDeps( arr )
 {
-  await fillTable( arr );
-  console.log( moduleDeps );
-}
+  Promise.all( createNameDepsObj( arr ) ).then( ( data ) =>
+  {
+    data.forEach( ( s, i ) =>
+    {
+      tableYML.forEach( ( l, i ) =>
+      {
+        if( l.N.name === s.name )
+        {
+          tableYML[ i ].Deps = s.deps;
+        }
+      } )
+    } )
 
-udpateDeps( moduleNames )
-// console.log( getDeps( 'sharp' ) );
+    tableYML = { 'Modules to read/write/convert/compress images' :  tableYML }
+
+    _.fileProvider.fileWrite( { filePath : abs( '../data/readWriteConvertCompressImg.yml' ), data : tableYML, encoding : 'yaml' } );
+  } )
+}
