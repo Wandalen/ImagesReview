206de4ef6292df024cd1539a3c829cf3b749d49d
Jackie <jackshel45@gmail.com>
2020-05-28 15:05:23 +0300

    Restructure into separate Compress & Convert tables

--- proto/Index.js
+++ proto/Index.js
@@ -1,10 +1,11 @@
 const _ = require( 'wTools' );
 require( 'wFiles' );
 
-const { 'Modules to read/write/convert/compress images' : rwTableObj } = readYML( '../data/ReadWriteConvertCompressImg.yml' );
+
 const { 'Modules to read images' : rTable } = readYML( '../data/ReadImg.yml' );
 const { 'Modules to write images' : wTable } = readYML( '../data/WriteImg.yml' );
-
+const { 'Modules to convert images' : convTable } = readYML( '../data/ConvertImg.yml' );
+const { 'Modules to compress images' : compTable } = readYML( '../data/CompressImg.yml' );
 const { 'Modules to process images' : processTableObj } = readYML( '../data/ProcessImg.yml' );
 
 const
@@ -17,11 +18,10 @@ const { Resources : resources } = readYML( '../data/Resources.yml' );
 
 let header1 = '# Awesome image!\n\nCurated overview of awesome Javascript projects to read / write / convert / compress /'
 let header2 = ' process images of different formats.\n'
-// let final = `${header1}${header2}\n${tableObjToMd( 1, rwTableObj )}\n\n${tableObjToMd( 1, rTable )}${readMd( `${__dirname}/../doc/RW.md` )}\n\n${tableObjToMd( 2, processTableObj )}\n
-// ${colsDefsToMd( columnsDef )}\n${algoToMd( algo )}\n${resourcesToMd( resources )}`;
 
-let final = `${header1}${header2}\n${tableObjToMd( 'read', rTable )}\n${tableObjToMd( 'write', wTable )}\n${readMd( `${__dirname}/../doc/RW.md` )}
-\n${tableObjToMd( 'process', processTableObj )}\n${colsDefsToMd( columnsDef )}\n${algoToMd( algo )}\n${resourcesToMd( resources )}`;
+let final = `${header1}${header2}\n${tableObjToMd( 'read', rTable )}\n${tableObjToMd( 'write', wTable )}
+${tableObjToMd( 'convert', convTable )}\n${tableObjToMd( 'compress', compTable )}
+${tableObjToMd( 'process', processTableObj )}\n${readMd( `${__dirname}/../doc/RW.md` )}\n\n${colsDefsToMd( columnsDef )}\n${algoToMd( algo )}\n${resourcesToMd( resources )}`;
 
 // Writing to file
 writeMd( '../README.md', final );
@@ -70,12 +70,6 @@ function tableObjToMd( table, obj )
 
   if( table === 'read' || table === 'write' )
   {
-    // temp = '### Modules to read/write/convert/compress images\n\n| **N** | **R** | **W** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps** |\n| --- | --- | --- | --- | --- | --- | -- | --- | --- | --- |\n';
-
-    // obj.forEach( ( el ) =>
-    // {
-    //   temp += `| [**${ el.N.name }**](${ el.N.link }) | ${el.R.join( ', ' )} | ${el.R.join( ', ' )} | ${ el.Code } | ${ el.Modular } | ${ el.I } | ${ el.PL } | ${ el[ 'B.s' ] } | ${ el[ 'N.s' ] } | ${ el.Deps } |\n`;
-    // } );
     temp = `### Modules to ${table === 'read' ? 'read': 'write'} images\n\n| **N** | ${table === 'read' ? '**Read**': '**Write**'} | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps** | **Working** |\n| --- | --- | --- | --- | --- | --- | -- | --- | --- | --- |\n`;
 
     obj.forEach( ( el ) =>
@@ -83,13 +77,13 @@ function tableObjToMd( table, obj )
       temp += `| [**${ el.N.name }**](${ el.N.link }) | ${table === 'read' ? el.Read.join( ', ' ) : el.Write.join( ', ' ) } | ${ el.Code } | ${ el.Modular } | ${ el.I } | ${ el.PL } | ${ el[ 'B.s' ] } | ${ el[ 'N.s' ] } | ${ el.Deps } | ${el.Working} |\n`;
     } );
   }
-  else if( table === 'compress' )
+  else if( table === 'compress' || table === 'convert' )
   {
-
-  }
-  else if( table === 'convert' )
-  {
-
+    temp = `### Modules to ${table === 'compress' ? 'compress': 'convert'} images\n\n| **N** | **R** | **W** | **Code** | **Modular** | **I** | **PL** | **B.s** | **N.s** | **Deps** | **Working** |\n| --- | --- | --- | --- | --- | --- | --- | -- | --- | --- | --- |\n`;
+    obj.forEach( ( el ) =>
+    {
+      temp += `| [**${ el.N.name }**](${ el.N.link }) | ${el.R[ 0 ] === '-'? '-': el.R.join( ', ' )} | ${el.W[ 0 ] === '-'? '-': el.W.join( ', ' )} | ${ el.Code } | ${ el.Modular } | ${ el.I } | ${ el.PL } | ${ el[ 'B.s' ] } | ${ el[ 'N.s' ] } | ${ el.Deps } | ${el.Working} |\n`;
+    } );
   }
   else if( table === 'process' )
   {
