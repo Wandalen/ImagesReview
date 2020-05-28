af867001df4d8c3b49f9684e538c9cab449e262e
Jackie <jackshel45@gmail.com>
2020-05-28 17:04:40 +0300

    Sorting 2

--- proto/Sort.js
+++ proto/Sort.js
@@ -2,20 +2,20 @@ const _ = require( 'wTools' );
 require( 'wFiles' );
 
 const { readYML, abs } = require( './Index' );
-const { 'Modules to read images' : rTable } = readYML( '../data/ReadImg.yml' );
-const { 'Modules to write images' : wTable } = readYML( '../data/WriteImg.yml' );
-const { 'Modules to convert images' : convTable } = readYML( '../data/ConvertImg.yml' );
-const { 'Modules to compress images' : compTable } = readYML( '../data/CompressImg.yml' );
-const { 'Modules to process images' : procTable } = readYML( '../data/ProcessImg.yml' );
+let { 'Modules to read images' : rTable } = readYML( '../data/ReadImg.yml' );
+let { 'Modules to write images' : wTable } = readYML( '../data/WriteImg.yml' );
+let { 'Modules to convert images' : convTable } = readYML( '../data/ConvertImg.yml' );
+let { 'Modules to compress images' : compTable } = readYML( '../data/CompressImg.yml' );
+let { 'Modules to process images' : procTable } = readYML( '../data/ProcessImg.yml' );
 let sortOrder = [ 'Working', 'Code', 'RW', 'Deps' ];
-// const sortOrder = [ 'Working', 'Code', 'Deps', 'RW' ];
+// const sortOrder = [ 'Working', 'Code', 'RW', 'Deps' ];
 
-let { 'Modules to read/write/convert/compress images' : rwTableArr } = readYML( '../data/ReadWriteConvertCompressImg.yml' );
-rwTableArr.sort( sortTable ).reverse();
-console.log( rwTableArr );
-rwTableArr = { 'Modules to read/write/convert/compress images' : rwTableArr };
+// let { 'Modules to read/write/convert/compress images' : rwTableArr } = readYML( '../data/ReadWriteConvertCompressImg.yml' );
+rTable.sort( sortTable ).reverse();
+console.log( rTable );
+rTable = { 'Modules to read images' : rTable };
 
-// _.fileProvider.fileWrite( { filePath : abs( '../data/ReadWriteConvertCompressImg.yml' ), data : rwTableArr, encoding : 'yaml' } );
+_.fileProvider.fileWrite( { filePath : abs( '../data/TESTReadImg.yml' ), data : rTable, encoding : 'yaml' } );
 
 function sortByWorking( a, b )
 {
@@ -68,8 +68,27 @@ function sortByCode ( a, b )
 function sortByRW( a, b )
 {
   // Sort by R.length + W.length
-  let aTotal = a.R.length + a.W.length;
-  let bTotal = b.R.length + b.W.length;
+  // let aTotal = a.R.length + a.W.length;
+  // let bTotal = b.R.length + b.W.length;
+  let aTotal, bTotal;
+
+  // Identify read/write table
+  if( a.Read )
+  {
+    aTotal = a.Read.length;
+    bTotal = b.Read.length;
+  }
+  else if( a.Write )
+  {
+    aTotal = a.Write.length;
+    bTotal = b.Write.length;
+  }
+  // Identify convert/compress table
+  else
+  {
+    aTotal = ( a.R[ 0 ]==='-'? 0: a.R.length ) + ( a.W[ 0 ]==='-'? 0: a.W.length );
+    bTotal = ( b.R[ 0 ]==='-'? 0: b.R.length ) + ( b.W[ 0 ]==='-'? 0: b.W.length )
+  }
 
   if( aTotal > bTotal )
   {
@@ -151,7 +170,7 @@ function sortByDeps( a, b )
   }
 }
 
-function sortReadTable( a, b )
+function sortTable( a, b )
 {
   if( sortOrder[ 0 ] === 'Working' )
   {
@@ -197,124 +216,141 @@ function sortReadTable( a, b )
           return sortByRW( a, b );
         }
       }
-      else if()
+      else if( sortOrder[ 1 ] === 'Deps' )
       {
-
+        if( sortByDeps( a, b ) === 0 )
+        {
+          if( sortOrder[ 2 ] ==='Code' )
+          {
+            const result = sortByCode( a, b ) === 0 ? sortByRW( a, b ) : sortByCode( a, b )
+            return result;
+          }
+          else if( sortOrder[ 2 ] ==='RW' )
+          {
+            const result = sortByRW( a, b ) === 0 ? sortByCode( a, b ) : sortByRW( a, b )
+            return result;
+          }
+        }
+        else
+        {
+          return sortByDeps( a, b );
+        }
       }
-
-
     }
     else
     {
       return sortByWorking( a, b );
     }
-
-
   }
-
-}
-
-function sortTable( a, b, table )
-{
-  if( table==='read' )
+  // Case when possible
+  else if( sortOrder[ 0 ] === 'Code' )
   {
-
+    return 100;
   }
 
-
-  // Sort Code -> R+W -> Deps
-  // if( sortOrder[ 0 ] === 'Code' )
-  // {
-  //   if( sortOrder[ 1 ] === 'RW' )
-  //   {
-  //     sortByCode( a, b, [ sortByRW, sortByDeps ] );
-  //   }
-  //   else if( sortOrder[ 1 ] === 'Deps' )
-  //   {
-  //     sortByCode( a, b, [ sortByDeps, sortByRW ] );
-  //   }
-  // }
-  // else if( sortOrder[ 0 ] === 'RW' )
-  // {
-  //   if( sortOrder[ 1 ] === 'Code' )
-  //   {
-  //     sortByRW( a, b, [ sortByCode, sortByDeps ] );
-  //   }
-  //   else if( sortOrder[ 1 ] === 'Deps' )
-  //   {
-  //     sortByRW( a, b, [ sortByDeps, sortByCode ] );
-  //   }
-  // }
-  // else if( sortOrder[ 0 ] === 'Deps' )
-  // {
-  //   if( sortOrder[ 1 ] === 'Code' )
-  //   {
-  //     sortByDeps( a, b, [ sortByCode, sortByRW ] );
-  //   }
-  //   else if( sortOrder[ 1 ] === 'RW' )
-  //   {
-  //     sortByDeps( a, b, [ sortByRW, sortByCode ] );
-  //   }
-  // }
-
-
-  // 1. Sort by open/closed code
-  // if( a.Code === 'open' && b.Code === 'closed' )
-  // {
-  //   return 1;
-  // }
-  // else if( a.Code === 'closed' && b.Code === 'open' )
-  // {
-  //   return -1;
-  // }
-  // else
-  // // 2. Sort by the amount of formats to read/write
-  // {
-  //   let aTotal = a.R.length + a.W.length;
-  //   let bTotal = b.R.length + b.W.length;
-
-  //   if( aTotal > bTotal )
-  //   {
-  //     return 1;
-  //   }
-  //   else if( aTotal < bTotal )
-  //   {
-  //     return -1
-  //   }
-  //   else
-  //   // 3. Sort the amount of dependents
-  //   {
-  //     let aDep, bDep;
-
-  //     if( a.Deps === '-' )
-  //     {
-  //       aDep = -1;
-  //     }
-  //     if( b.Deps === '-' )
-  //     {
-  //       bDep = -1;
-  //     }
-  //     if( a.Deps !== '-' )
-  //     {
-  //       aDep = +a.Deps;
-  //     }
-  //     if( b.Deps !== '-' )
-  //     {
-  //       bDep = +b.Deps;
-  //     }
-
-  //     if( aDep > bDep )
-  //     {
-  //       return 1;
-  //     }
-  //     else if( aDep < bDep )
-  //     {
-  //       return -1;
-  //     }
-  //     else
-  //     {
-  //       return 0;
-  //     }
-  //   }
-  // }
 }
+
+// function sortTable( a, b, table )
+// {
+//   if( table==='read' )
+//   {
+
+//   }
+
+
+// Sort Code -> R+W -> Deps
+// if( sortOrder[ 0 ] === 'Code' )
+// {
+//   if( sortOrder[ 1 ] === 'RW' )
+//   {
+//     sortByCode( a, b, [ sortByRW, sortByDeps ] );
+//   }
+//   else if( sortOrder[ 1 ] === 'Deps' )
+//   {
+//     sortByCode( a, b, [ sortByDeps, sortByRW ] );
+//   }
+// }
+// else if( sortOrder[ 0 ] === 'RW' )
+// {
+//   if( sortOrder[ 1 ] === 'Code' )
+//   {
+//     sortByRW( a, b, [ sortByCode, sortByDeps ] );
+//   }
+//   else if( sortOrder[ 1 ] === 'Deps' )
+//   {
+//     sortByRW( a, b, [ sortByDeps, sortByCode ] );
+//   }
+// }
+// else if( sortOrder[ 0 ] === 'Deps' )
+// {
+//   if( sortOrder[ 1 ] === 'Code' )
+//   {
+//     sortByDeps( a, b, [ sortByCode, sortByRW ] );
+//   }
+//   else if( sortOrder[ 1 ] === 'RW' )
+//   {
+//     sortByDeps( a, b, [ sortByRW, sortByCode ] );
+//   }
+// }
+
+
+// 1. Sort by open/closed code
+// if( a.Code === 'open' && b.Code === 'closed' )
+// {
+//   return 1;
+// }
+// else if( a.Code === 'closed' && b.Code === 'open' )
+// {
+//   return -1;
+// }
+// else
+// // 2. Sort by the amount of formats to read/write
+// {
+//   let aTotal = a.R.length + a.W.length;
+//   let bTotal = b.R.length + b.W.length;
+
+//   if( aTotal > bTotal )
+//   {
+//     return 1;
+//   }
+//   else if( aTotal < bTotal )
+//   {
+//     return -1
+//   }
+//   else
+//   // 3. Sort the amount of dependents
+//   {
+//     let aDep, bDep;
+
+//     if( a.Deps === '-' )
+//     {
+//       aDep = -1;
+//     }
+//     if( b.Deps === '-' )
+//     {
+//       bDep = -1;
+//     }
+//     if( a.Deps !== '-' )
+//     {
+//       aDep = +a.Deps;
+//     }
+//     if( b.Deps !== '-' )
+//     {
+//       bDep = +b.Deps;
+//     }
+
+//     if( aDep > bDep )
+//     {
+//       return 1;
+//     }
+//     else if( aDep < bDep )
+//     {
+//       return -1;
+//     }
+//     else
+//     {
+//       return 0;
+//     }
+//   }
+// }
+// }
