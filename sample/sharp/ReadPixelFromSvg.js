a0a82e5f73294b89bd78d3375af2480ba46fe7f8
Jackie <jackshel45@gmail.com>
2020-06-02 16:23:16 +0300

    Minor

--- sample/sharp/ReadPixelFromSvg.js
+++ sample/sharp/ReadPixelFromSvg.js
@@ -1,6 +1,6 @@
 const sharp = require( 'sharp' );
 
-sharp( `${__dirname}/../../data/images/Image.svg` ).raw()
+sharp( `${__dirname}/../../data/images/Image2.svg` ).raw()
 .toBuffer( { resolveWithObject : true } )
 .then( ( { data } ) => console.log( 'rgb(' + data.toJSON().data.slice( 0, 3 ) + ')' ) );
 // OUTPUT: rgb(0,0,0)
