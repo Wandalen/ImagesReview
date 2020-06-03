d3462826c873439de3f2d13f9836553014fbc674
Jackie <jackshel45@gmail.com>
2020-06-03 11:28:17 +0300

    test

--- sample/gm/ReadPixelFromJpg.js
+++ sample/gm/ReadPixelFromJpg.js
@@ -1,8 +1,8 @@
-const gm = require( 'gm' ).subClass( { imageMagick : true } );
+// const gm = require( 'gm' ).subClass( { imageMagick : true } );
 
-gm( `${__dirname}/../../data/images/Image.jpg` ).identify( ( err, data ) =>
-{
-    if( err ) console.log( err );
-    console.log( data.Histogram[ '1' ][ 2 ] )
-} );
-// OUTPUT: (249,0,0) #F90000 srgb(249,0,0)
+// gm( `${__dirname}/../../data/images/Image.jpg` ).identify( ( err, data ) =>
+// {
+//     if( err ) console.log( err );
+//     console.log( data.Histogram[ '1' ][ 2 ] )
+// } );
+// // OUTPUT: (249,0,0) #F90000 srgb(249,0,0)
--- sample/gm/ReadPixelFromPng.js
+++ sample/gm/ReadPixelFromPng.js
@@ -1,8 +1,8 @@
-const gm = require( 'gm' ).subClass( { imageMagick : true } );
+// const gm = require( 'gm' ).subClass( { imageMagick : true } );
 
-gm( `${__dirname}/../../data/images/Image.png` ).identify( ( err, data ) =>
-{
-    if( err ) console.log( err );
-    console.log( data.Histogram[ '1' ][ 2 ] )
-} );
-// OUTPUT: (255,0,0,255) #FF0000FF red
+// gm( `${__dirname}/../../data/images/Image.png` ).identify( ( err, data ) =>
+// {
+//     if( err ) console.log( err );
+//     console.log( data.Histogram[ '1' ][ 2 ] )
+// } );
+// // OUTPUT: (255,0,0,255) #FF0000FF red
