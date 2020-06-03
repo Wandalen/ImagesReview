47a9719968b85a660eb47d7469c8f76fea1e7ac2
Jackie <jackshel45@gmail.com>
2020-06-03 15:35:11 +0300

    samples

--- sample/jpeg-lossless-decoder-js/ReadPixelFromJpg.js
+++ sample/jpeg-lossless-decoder-js/ReadPixelFromJpg.js
@@ -1,6 +1,6 @@
-// const fs = require( 'fs' )
-// const jpeg = require( 'jpeg-lossless-decoder-js' );
-// const decoder = new jpeg.lossless.Decoder();
-// const output = decoder.decompress( fs.readFileSync( `${__dirname}/../../data/images/Image.jpg` ) );
+const fs = require( 'fs' )
+const jpeg = require( 'jpeg-lossless-decoder-js' );
+const decoder = new jpeg.lossless.Decoder();
+const output = decoder.decompress( fs.readFileSync( `${__dirname}/../../data/images/Image.jpg` ), 1, 4 );
 
-// console.log( output )
+console.log( output )
