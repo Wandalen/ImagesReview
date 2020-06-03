708dbc68ba3cb3098da06dbd74fb9f387d1c8157
Jackie <jackshel45@gmail.com>
2020-06-03 17:03:28 +0300

    gm fixes 2

--- sample/gm/WritePixels.js
+++ sample/gm/WritePixels.js
@@ -1,12 +1,12 @@
-// const gm = require( 'gm' ).subClass( { imageMagick : true } );
+const gm = require( 'gm' ).subClass( { imageMagick : true } );
 
-// let buf = Buffer.from( {
-//   type : 'Buffer',
-//   data : [
-//     255, 0, 0, 255, 0, 255,
-//     0, 255, 0, 0, 255, 255,
-//     255, 255, 255, 255
-//   ]
-// } );
+let buf = Buffer.from( {
+  type : 'Buffer',
+  data : [
+    255, 0, 0, 255, 0, 255,
+    0, 255, 0, 0, 255, 255,
+    255, 255, 255, 255
+  ]
+} );
 
-// gm( 2, 2, buf ).write( `${__dirname}/../../data/images/WrittenPixelsGm.png`, ( err ) => console.log( err ) )
+gm( 2, 2, buf ).write( `${__dirname}/../../data/images/WrittenPixelsGm.png`, ( err ) => console.log( err ) )
