66f4c9e34fa79858b61e8f7a8fae079c0ea303dd
Jackie <jackshel45@gmail.com>
2020-06-03 12:25:58 +0300

    comment out uncompleted samples

--- sample/jpeg-lossless-decoder-js/ReadPixelFromJpg.js
+++ sample/jpeg-lossless-decoder-js/ReadPixelFromJpg.js
@@ -1,6 +1,6 @@
-const fs = require( 'fs' )
-const jpeg = require( 'jpeg-lossless-decoder-js' );
-const decoder = new jpeg.lossless.Decoder();
-const output = decoder.decompress( fs.readFileSync( `${__dirname}/../../data/images/Image.jpg` ) );
+// const fs = require( 'fs' )
+// const jpeg = require( 'jpeg-lossless-decoder-js' );
+// const decoder = new jpeg.lossless.Decoder();
+// const output = decoder.decompress( fs.readFileSync( `${__dirname}/../../data/images/Image.jpg` ) );
 
-console.log( output )
+// console.log( output )
--- sample/omggif/ReadPixelFromGif.js
+++ sample/omggif/ReadPixelFromGif.js
@@ -1,5 +1,5 @@
-const fs = require( 'fs' );
-const omggif = require( 'omggif' );
+// const fs = require( 'fs' );
+// const omggif = require( 'omggif' );
 
-const gr = new omggif.GifReader( fs.readFileSync( `${__dirname}/../../data/images/Image.gif` ) );
-console.log( gr.frameInfo( 0 ) )
+// const gr = new omggif.GifReader( fs.readFileSync( `${__dirname}/../../data/images/Image.gif` ) );
+// console.log( gr.frameInfo( 0 ) )
