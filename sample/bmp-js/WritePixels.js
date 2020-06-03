5795a0991395f5aaa6a50680915e53137dddbdad
Jackie <jackshel45@gmail.com>
2020-06-03 16:54:03 +0300

    fixes

---
+++ sample/bmp-js/WritePixels.js
@@ -0,0 +1,21 @@
+const bmp = require( 'bmp-js' );
+const fs = require( 'fs' );
+
+let buf = Buffer.from( {
+  type : 'Buffer',
+  data : [
+    255, 0, 0, 255, 0, 255,
+    0, 255, 0, 0, 255, 255,
+    255, 255, 255, 255
+  ]
+} );
+
+
+const bmpData = {
+  data : buf,
+  width : 2,
+  height : 2
+};
+const rawData = bmp.encode( bmpData );
+
+fs.writeFileSync( `${__dirname}/../../data/images/WrittenPixelsBmp-js.bmp`, rawData.data );
--- sample/sharp/WritePixels.js
+++
@@ -1,3 +0,0 @@
-const sharp = require( 'sharp' );
-
-sharp( `${__dirname}/../../data/images/Image.png` ).toFile( `${__dirname}/../../data/images/WrittenPixelsSharp.png` )
