4e99995380ca140d060ab2cf76f72661c1d41e61
Jackie <jackshel45@gmail.com>
2020-06-03 11:20:22 +0300

    minor

--- sample/sharp/WritePixels.js
+++ sample/sharp/WritePixels.js
@@ -1,26 +1,26 @@
-const sharp = require( 'sharp' );
+// const sharp = require( 'sharp' );
 
-let buf = Buffer.from( {
-  type : 'Buffer',
-  data : [
-    255, 0, 0, 255, 0, 255,
-    0, 255, 0, 0, 255, 255,
-    255, 255, 255, 255
-  ]
-} );
+// let buf = Buffer.from( {
+//   type : 'Buffer',
+//   data : [
+//     255, 0, 0, 255, 0, 255,
+//     0, 255, 0, 0, 255, 255,
+//     255, 255, 255, 255
+//   ]
+// } );
 
-const input  =
-{
-  data : buf,
-  info : {
-    format : 'raw',
-    width : 2,
-    height : 2,
-    channels : 4,
-    premultiplied : false,
-    size : 16
-  }
-}
+// const input  =
+// {
+//   data : buf,
+//   info : {
+//     format : 'raw',
+//     width : 2,
+//     height : 2,
+//     channels : 4,
+//     premultiplied : false,
+//     size : 16
+//   }
+// }
 
 // {
 //   data: <Buffer ff 00 00 ff 00 ff 00 ff 00 00 ff ff ff ff ff ff>,
@@ -40,6 +40,6 @@ const input  =
 //   255, 255, 255
 // ] );
 
-console.log( buf )
+// console.log( buf )
 
 // sharp( buf ).toFile( `${__dirname}/../../data/images/WrittenPixelsSharp.png`, ( err ) => console.log( err ) );
