353678800b8484f95008bb0c5a325bf8916ad33c
Jackie <jackshel45@gmail.com>
2020-06-03 16:07:05 +0300

    add sample

--- sample/bmp-js/ReadPixelFromBmp.js
+++ sample/bmp-js/ReadPixelFromBmp.js
@@ -1,7 +1,9 @@
-// const bmp = require( 'bmp-js' );
-// const fs = require( 'fs' );
+const bmp = require( 'bmp-js' );
+const fs = require( 'fs' );
 
-// const bmpBuffer = fs.readFileSync( `${__dirname}/../../data/images/Image.bmp` );
-// const bmpData = bmp.decode( bmpBuffer );
-// console.log( 'rgb(' + bmpData.data.toJSON().data.slice( 0, 3 ) + ')' )
-// OUTPUT: rgb(0,0,0)
+const bmpBuffer = fs.readFileSync( `${__dirname}/../../data/images/Image.bmp` );
+const bmpData = bmp.decode( bmpBuffer );
+let data = bmpData.data.toJSON().data
+data = [ data[ 3 ], data[ 2 ], data[ 1 ] ];
+console.log( 'rgb(' + data + ')' );
+// OUTPUT: rgb(255,0,0)
--- sample/omggif/WritePixels.js
+++ sample/omggif/WritePixels.js
@@ -1,15 +1,15 @@
-const fs = require( 'fs' );
-const omggif = require( 'omggif' );
+// const fs = require( 'fs' );
+// const omggif = require( 'omggif' );
 
-let buf = Buffer.from( {
-  type : 'Buffer',
-  data : [
-    255, 0, 0, 255, 0, 255,
-    0, 255, 0, 0, 255, 255,
-    255, 255, 255, 255
-  ]
-} );
+// let buf = Buffer.from( {
+//   type : 'Buffer',
+//   data : [
+//     255, 0, 0, 255, 0, 255,
+//     0, 255, 0, 0, 255, 255,
+//     255, 255, 255, 255
+//   ]
+// } );
 
-let gf = new omggif.GifWriter( buf, 2, 2, { palette : [ 0xff0000, 0x0000ff ] } );
+// let gf = new omggif.GifWriter( buf, 2, 2, { palette : [ 0xff0000, 0x0000ff ] } );
 
-console.log( gf );
+// console.log( gf );
