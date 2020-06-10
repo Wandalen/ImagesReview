3e775d6887ef5bda3203ec55b7f5251fb7d25db9
Jackie <jackshel45@gmail.com>
2020-06-10 11:50:41 +0300

    minor

--- sample/jpeg-lossless-decoder-js/ReadPixelFromJpg.js
+++
@@ -1,6 +0,0 @@
-const fs = require( 'fs' )
-const jpeg = require( 'jpeg-lossless-decoder-js' );
-const decoder = new jpeg.lossless.Decoder();
-const output = decoder.decompress( fs.readFileSync( `${__dirname}/../../data/images/Image.jpg` ), 1000102010200, 1012312312300 );
-
-console.log( Buffer.from( output ).toJSON() )
