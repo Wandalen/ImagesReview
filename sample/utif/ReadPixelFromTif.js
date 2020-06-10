886301603fb2e353d5cf05a1a9e471d00d5a5abf
Jackie <jackshel45@gmail.com>
2020-06-10 11:02:25 +0300

    Adde utif sample reader

--- sample/utif/ReadPixelFromTif.js
+++ sample/utif/ReadPixelFromTif.js
@@ -2,8 +2,6 @@ const fs = require( 'fs' );
 const UTIF = require( 'utif' );
 
 const ifds = UTIF.decode( fs.readFileSync( `${__dirname}/../../data/images/Image.tif` ) );
-console.log( ifds );
-// const sss = UTIF.decodeImage( fs.readFileSync( `${__dirname}/../../data/images/Image.tif` ), ifds[ 0 ] )
-// const rgba  = UTIF.toRGBA8( ifds[ 0 ] );  // Uint8Array with RGBA pixels
-// console.log( ifds[ 0 ].width, ifds[ 0 ].height, ifds[ 0 ] );
-// console.log( rgba )
+UTIF.decodeImage( fs.readFileSync( `${__dirname}/../../data/images/Image.tif` ), ifds[ 0 ] )
+console.log( 'rgb( ' + ifds[ 0 ].data.slice( 0, 3 ) + ' )' );
+// log: rgb( 255,0,0 )
