96fcc30c38171d09bc1b1bfe8d63bba077502699
Jackie <jackshel45@gmail.com>
2020-06-15 09:23:12 +0300

    add webp image

--- sample/sharp/ReadPixelFromWebp.js
+++ sample/sharp/ReadPixelFromWebp.js
@@ -3,4 +3,4 @@ const sharp = require( 'sharp' );
 sharp( `${__dirname}/../../data/images/Image.webp` ).raw()
 .toBuffer( { resolveWithObject : true } )
 .then( ( { data } ) => console.log( 'rgb( ' + data.toJSON().data.slice( 0, 3 ) + ' )' ) );
-// log: rgb( 83,83,83 )
+// log: rgb( 254,0,0 )
